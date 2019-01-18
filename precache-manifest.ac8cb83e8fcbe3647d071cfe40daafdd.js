69cad8d0e7b4acb9743f700cd0308ca2e970c116
lamartire <lamartire@gmail.com>
2019-01-18 18:21:50 +0300

    Updates

---
+++ precache-manifest.ac8cb83e8fcbe3647d071cfe40daafdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4115f912676fa0536c9b",
+    "url": "/web3-debugger-dapp/static/js/runtime~main.4115f912.js"
+  },
+  {
+    "revision": "5130a15aa992911a4b6b",
+    "url": "/web3-debugger-dapp/static/js/main.5130a15a.chunk.js"
+  },
+  {
+    "revision": "7ba799164707296d3103",
+    "url": "/web3-debugger-dapp/static/js/1.7ba79916.chunk.js"
+  },
+  {
+    "revision": "5130a15aa992911a4b6b",
+    "url": "/web3-debugger-dapp/static/css/main.dad2c995.chunk.css"
+  },
+  {
+    "revision": "c5d866ae119ac545bd4269d29dbf701c",
+    "url": "/web3-debugger-dapp/index.html"
+  }
+];
